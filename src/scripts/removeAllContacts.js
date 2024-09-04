72e6661de3fead489429867e08270447f24fc7e0
iahub4metra <buhai.artem7@gmail.com>
2024-09-04 15:27:21 +0100

    Update removeAllContacts.js

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,24 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    let contacts = []
+    try {
+        const data = await readContacts();
+        
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+
+    } catch (error) {
+        console.error('Error reading or parsing db.json:', error);
+    }
+    contacts = []
+    try {
+        await writeContacts(JSON.stringify(contacts, null, 2));
+    } catch (error) {
+        console.error('Error writing to db.json:', error);
+    }
+};
 
 removeAllContacts();
