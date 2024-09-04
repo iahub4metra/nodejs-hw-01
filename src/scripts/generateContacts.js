c2fa304dbfaed37e9b6a9018186e3b1b82f32f7c
iahub4metra <buhai.artem7@gmail.com>
2024-09-04 15:27:18 +0100

    Update generateContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,32 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+const generateContacts = async (number) => {
+    let contacts = [];
+
+    try {
+        const data = await readContacts();
+        
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+
+    } catch (error) {
+        console.error('Error reading or parsing db.json:', error);
+    }
+
+    for (let i = 0; i < number; ++i) {
+        const newContact = createFakeContact();
+        contacts.push(newContact);
+    }
+
+    try {
+        await writeContacts(JSON.stringify(contacts, null, 2))
+    } catch (error) {
+        console.error('Error writing to db.json:', error);
+    }
+
+
+};
 
 generateContacts(5);
