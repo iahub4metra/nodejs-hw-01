16525bc8b86beef3885b10ba9f0202f5cdac1f43
iahub4metra <buhai.artem7@gmail.com>
2024-09-04 15:27:13 +0100

    Update addOneContact.js

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,26 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    let contacts = []
+    try {
+        const data = await readContacts();
+        
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+
+    } catch (error) {
+        console.error('Error reading or parsing db.json:', error);
+    }
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+    try {
+        await writeContacts(JSON.stringify(contacts, null, 2)) 
+    } catch (error) {
+        console.error('Error writing to db.json:', error);
+    }
+};
 
 addOneContact();
