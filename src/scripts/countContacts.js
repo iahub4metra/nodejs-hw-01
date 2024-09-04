5506532b60989b0ff3876e32405f92b0a1ecddbc
iahub4metra <buhai.artem7@gmail.com>
2024-09-04 15:27:16 +0100

    Update countContacts.js

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,18 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+    let contacts = []
+    try {
+        const data = await readContacts();
+        
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+
+    } catch (error) {
+        console.error('Error reading or parsing db.json:', error);
+    }
+    return contacts.length
+};
 
 console.log(await countContacts());
