1438d94b499e8c3fa40af64b22494dff8c6dc4f5
iahub4metra <buhai.artem7@gmail.com>
2024-09-04 15:27:19 +0100

    Update getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,18 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const getAllContacts = async () => {
+    let contacts = []
+    try {
+        const data = await readContacts();
+        
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+
+    } catch (error) {
+        console.error('Error reading or parsing db.json:', error);
+    }
+    return contacts
+};
 
 console.log(await getAllContacts());
